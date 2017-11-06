1ddc3e7fd96ae61ca2d49eea836402cff32690e2
barnabas <bpravin8@yahoo.com>
2017-11-05 22:59:26 -0500

    worked on the new gekko page. rearranged market and picking gekko

--- gekko-stable/gekko-stable/web/vue/src/components/global/configbuilder/typepicker.vue
+++ gekko-stable/gekko-stable/web/vue/src/components/global/configbuilder/typepicker.vue
@@ -1,21 +1,21 @@
 <template lang='jade'>
 div
-  h3.center3 Gekko Type
+  h3.center3.header_font How Do You Want To Run Gekko?
     .tooltip2
           img(src='/assets/Question.jpg', width='20', height='20')
           span.tooltiptext2 Once you have a strategy you can use Gekko to automatically run it in a few different ways
   hr
   template
-    label(for='type').wrapper How do you want to run Gekko?
-    form.radio.grd
-      div.grd-row(v-for='(type, i) in types').m1
-        input.grd-row-col-1-6(type='radio', :value='i', v-model='selectedTypeIndex')
-        label.grd-row-col-5-6(:for='i') {{ type }}
+    label(for='type').wrapper
+    form.radio.grd.grd-row
+      div.grd-row-col-3-6(v-for='(type, i) in types').m1
+        label.radio_header(:for='i') {{ type }}
           .tooltip2
             img(src='/assets/Question.jpg', width='10', height='10')
             span.tooltiptext2(v-if="type === 'paper trader'"){{paperData}}
             span.tooltiptext2(v-if="type === 'market watcher'") {{marketData}}
             span.tooltiptext2(v-if="type === 'tradebot'") {{botData}}
+        input.radio_button_margins(type='radio', :value='i', v-model='selectedTypeIndex')
           
                 
 </template>
@@ -69,12 +69,25 @@ export default {
 
 <style>
 
+.radio_header{
+  font-size: 1rem;
+text-transform: uppercase;
+}
+
+.header_font{
+  font-size: 1.4rem;
+}
+
 .radio label {
   margin-top: 0;
 }
 
 .center3{
   text-align: center;
+}
+
+.radio_button_margins{
+  margin-left: 50%;
 }
 
 </style>
