67b212e08ab7d75216f3b11e00710cbef4f41b14
Bryan Beck <bbeck1@students.towson.edu>
2017-11-07 13:51:42 -0500

    removed comments from Install_Gekko.Bat

--- Install_Gekko.bat
+++ Install_Gekko.bat
@@ -2,7 +2,7 @@
 ::start /wait msiexec.exe /i %~dp0node-v6.11.5-x64.msi /L*V "%Temp%\nodejsv6.log
 CD %~dp0
 copy GekkoTU.lnk %USERPROFILE%\Desktop
-::CD %~dp0gekko-stable
-::CD gekko-stable
-::npm install --only=production
+CD %~dp0gekko-stable
+CD gekko-stable
+npm install --only=production
 pause
